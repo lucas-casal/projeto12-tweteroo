784d2d071542886b229c6aa7b3735d83c76bb2a6
Lucas Casal <lucasocteixeira@gmail.com>
2023-06-22 22:59:37 -0300

    corrections

--- src/app.js
+++ src/app.js
@@ -4,15 +4,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const users = [{
-	username: 'bobesponja', 
-	avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
-}]
+const users = []
 
-const tweets = [{
-    username: "bobesponja",
-    tweet: "Eu amo hambúrguer de siri!"
-}]
+const tweets = []
 
 app.post('/sign-up', (req, res)=>{
     const { username, avatar } = req.body
